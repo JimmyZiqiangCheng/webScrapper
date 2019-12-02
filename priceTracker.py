c193aba401f2ebd75869db1432357db0324e5fbb
Ziqiang Cheng <ziqiangcheng@Ziqiangs-MacBook-Pro.local>
2019-12-02 22:44:11 +1030

    modified the code to make it more readable

--- priceTracker.py
+++ priceTracker.py
@@ -1,11 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 import smtplib
+from re import sub
+from decimal import Decimal
 
 def checkPrice():
     # need to find the product link and id for the price (it may change)
-    URL = 'https://www.amazon.com.au/Soniq-N65UX17A-AU-Google-Chromecast-Built/dp/B07JHYD5R4?smid=ANEGB3WVEVKZB&pf_rd_p=407520f4-63f9-4772-91bf-5ffe50123934&pf_rd_r=HEBK96XGBHD7J5E6S743'
-    ID = 'priceblock_ourprice'
+    URL = 'https://www.ebay.com.au/itm/Sony-75-X95G-Full-Array-LED-4K-Ultra-HD-Smart-Android-TV-Box-Damaged/283668292759?epid=23034117006&hash=item420bf2a497%3Ag%3Aj1wAAOSwqIldxP1Z&_trkparms=%2526rpp_cid%253D5dc15db70f926b2ecea2adef'
+    ID = 'prcIsum'
+    # put your target price here
+    PRICE_TARGET = 500
 
     headers = {
         "User-Agent":
@@ -14,33 +18,36 @@ def checkPrice():
     page = requests.get(URL, headers = headers) 
 
     soup = BeautifulSoup(page.content, 'html.parser')
-    soup2 = BeautifulSoup(soup.prettify(), 'html.parser')
 
-    price= soup2.find(id=ID).to_text()
-    converted_price = float(price[1:])
+    price= soup.find(id=ID).get_text()
+    converted_price = Decimal(sub(r'[^\d.]', '', price[3:]))
 
-    if (converted_price > 500):
-        send_email()
+    print(converted_price)
 
-def send_email():
+    if (converted_price < PRICE_TARGET):
+        send_email(converted_price)
+
+def send_email(price):
+    EMAIL_ADDRESS = 'username@gmail.com'
     server = smtplib.SMTP('smtp.gmail.com', 857)
     server.ehlo()
     server.starttls()
     server.ehlo()
 
-    server.login('username@gmail.com', '2-step authentication generated password')
+    server.login(EMAIL_ADDRESS, '2-step authentication generated password')
     subject = 'Price Reached Ideal Range'
     body = 'Check the link https://www.amazon.com.au/Soniq-N65UX17A-AU-Google-Chromecast-Built/dp/B07JHYD5R4?smid=ANEGB3WVEVKZB&pf_rd_p=407520f4-63f9-4772-91bf-5ffe50123934&pf_rd_r=HEBK96XGBHD7J5E6S743'
 
     msg = f"Subject:{subject}\n\n{body}"
 
     server.sendmail(
-        'email1@gmail.com',
-        'username@gmail.com',
+        EMAIL_ADDRESS,
+        EMAIL_ADDRESS,
         msg
     )
     
-    print('price Drop and email has been sent')
+    print('price Drop has dropped to: ', price)
+    print('email has been sent to,')
     server.quit()
 
 checkPrice()
--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
 **Price Tracker**
 
-This is a simple item price tracker for tracking down your favorite items on sites like Amazon.
+This is a simple item price tracker for tracking down your favorite items on sites like Ebay.
+Might not work on Amazon due to their extra protection against web scraping.
